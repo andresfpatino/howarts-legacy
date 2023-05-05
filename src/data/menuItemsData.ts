245bf8d07f53fb78277de4d3cfca99e3b4d84f6c
Andrés Patiño <andres@frontporchsolutions.com>
2023-05-04 21:46:55 -0500

    feat: :construction: Component menu

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,18 @@
+import type { MenuItem } from "../types/types";
+
+const menuItems:MenuItem[] = [
+    {
+        title: 'Home',
+        url: '/'
+    },
+    {
+        title: 'Faqs',
+        url: '/faqs'
+    },
+    {
+        title: 'Blog',
+        url: '/blog'
+    }
+];
+
+export default menuItems;
